6ac8466b4c12355b31c8d913e22da8f89a6fcb8f
jadmin <gerald.chen.hz@gmail.com>
2017-06-16 09:28:06 +0800

    ccccc

--- src/main/java/com/github/javaclub/toolbox/util/UuidUtil.java
+++ src/main/java/com/github/javaclub/toolbox/util/UuidUtil.java
@@ -111,7 +111,7 @@ public abstract class UuidUtil {
 	private static final int JVM = (int) ( System.currentTimeMillis() >>> 8 );
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10000; i++) {
 			System.out.println(uuid());
 		}
 	}
